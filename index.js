7de4caa53ea5e6dd068c984d19cd9f12e20fe34d
Justin Dial <justinalandial@gmail.com>
2018-08-02 13:13:09 -0500

    update

--- index.js
+++ index.js
@@ -1,81 +1,20 @@
-window.onload = function () {
-  new Vue({
-    el: '#app',
-    data: {
-      currentSort:'name',
-      currentSortDir:'asc',
-      search: '',
-      columns: ["Player","Team","Position"],
-      pos:{'DT':'p20'
-          ,'DL':'p20'
-          ,'DE':'p21'
-          ,'DB':'p21'
-          ,'LB':'p22'
-          ,'CB':'p23'
-          ,'S':'p24'
-          ,'QB':'p25'
-          ,'RB':'p26'
-          ,'WR':'p27'
-          ,'TE':'p24'
-          ,'C':'p25'
-          ,'G':'p26'
-          ,'T':'p27'
-          ,'K':'p28'
-          ,'H':'p29'
-          ,'LS':'p30'
-          ,'P':'p31'
-          ,'KOS':'p32'
-          ,'PR':'p33'
-          ,'KR':'p34'
-        },
-      people:[],
-      descs:[]
-    },
-    mounted () {
-      console.log('getting data');
-      axios.get('http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2017&week=1&format=json')
-        .then(response => {
-          this.people = response.data.players;
-        });
-    },
-    methods: {
-      sort:function(s) {
-        if(s === this.currentSort) {
-          this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
-        }
-        this.currentSort = s;
-      },
-      getDesc:function(id){
-        var notes='';
-        axios.get('http://api.fantasy.nfl.com/v1/players/details?playerId='+id)
-          .then(response => {
-            try {
-              for (var note in response.data.players[0].notes){
-                notes = notes +'\n'+note+'--'+response.data.players[0].notes[note].body;
-              }
-              alert(notes);
-            }
-            catch(err) {
-              return 'N/A';
-            }
-          });
-      }
-    },
-    computed: {
-      filteredlist:function()
-      {
-      	 var self=this;
-         return this.people.filter(function(item){return item.name.toLowerCase().indexOf(self.search.toLowerCase())>=0;});
-      },
-      sortedtable:function() {
-        return this.filteredlist.sort((a,b) => {
-          let modifier = 1;
-          if(this.currentSortDir === 'desc') modifier = -1;
-          if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
-          if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
-          return 0;
-        });
-      }
-    }
-  })
-};
+const path = require('path');
+
+const http = require('http').Server(app);
+
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+//Index Route
+app.get("*", function(req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(ip+" - - "+moment().format('MM/DD/YY hh:mm:ss')+" "+req.route.path);
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+// Initialize app
+http.listen(port, () => console.log('App Started'));
