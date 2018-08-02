a01f976f739142c91a0e1f8051162e8f2d8c3b5d
Justin Dial <justinalandial@gmail.com>
2018-08-02 13:33:33 -0500

    css

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 8080;
 
 
-
+app.use(express.static(__dirname + '/css' ));
 //Index Route
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, "./index.html"));
