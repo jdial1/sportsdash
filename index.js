986940cf74fd8a049da22bbf15e5d7403c98c5d1
Justin Dial <justinalandial@gmail.com>
2018-08-02 13:21:25 -0500

    f

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const path = require('path');
-const http = require('http').Server(app);
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
 
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
 
 //Index Route
-app.get("*", function(req, res) {
+app.get('/', (request, response) => {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(ip+" - - "+moment().format('MM/DD/YY hh:mm:ss')+" "+req.route.path);
   res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 // Initialize app
-http.listen(port, () => console.log('App Started'));
+app.listen(port, () => console.log('App Started'));
